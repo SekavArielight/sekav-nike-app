74cdbd5156f89e97fdbec04230f5fc70f9860878
God'swill Sekav Apya <godswillapya@gmail.com>
2024-12-25 12:07:36 +0100

    Customer Reviews Section Build

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { reviews } from "../constants";
+import ReviewCard from "../components/ReviewCard";
 
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
-  )
-}
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Are Our <span className="text-coral-red">Customers</span> Saying?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Hear genuine stories from our satisfied customers about their
+        exceptional experiences with us.
+      </p>
 
-export default CustomerReviews
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review) => (
+          <ReviewCard 
+            key={review.customerName}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default CustomerReviews;
